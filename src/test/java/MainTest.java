b83b644188b1404250cee7444d96453af3bd0cd7
ilnarsafigullin <shafigullin.ik@gmail.com>
2021-07-22 12:39:37 +0300

    Add message to test case

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,6 +5,7 @@ public class MainTest {
 
     @Test
     public void testSum(){
-        Assertions.assertEquals(7, Main.sum(2, 5));
+        int actualValue = Main.sum(2, 5);
+        Assertions.assertEquals(7, actualValue, "Ваш метод выдает 2 + 5 = " + actualValue);
     }
 }
