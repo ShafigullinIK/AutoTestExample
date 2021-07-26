3c2e70a609247dc9cbd13b13a076a9e28fcbb0e9
ilnarsafigullin <shafigullin.ik@gmail.com>
2021-07-26 08:08:52 +0300

    fix testcase with negative value again)

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,6 +8,6 @@ public class MainTest {
         int actualValue = Main.sum(4, 5);
         Assertions.assertEquals(9, actualValue, "Ваш метод выдает 4 + 5 = " + actualValue);
         int actualValueForNegative = Main.sum(-2, 5);
-        Assertions.assertEquals(3, actualValue, "Ваш метод выдает -2 + 5 = " + actualValueForNegative);
+        Assertions.assertEquals(3, actualValueForNegative, "Ваш метод выдает -2 + 5 = " + actualValueForNegative);
     }
 }
