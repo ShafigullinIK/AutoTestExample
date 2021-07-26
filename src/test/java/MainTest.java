6d39b1f6bd409796c15b7024e9f26c209f8e0295
ilnarsafigullin <shafigullin.ik@gmail.com>
2021-07-26 08:06:08 +0300

    add one more test with negative value

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 public class MainTest {
 
     @Test
-    public void testSum(){
+    public void testSum() {
         int actualValue = Main.sum(4, 5);
         Assertions.assertEquals(9, actualValue, "Ваш метод выдает 4 + 5 = " + actualValue);
+        int actualValueForNegative = Main.sum(-2, 5);
+        Assertions.assertEquals(3, actualValue, "Ваш метод выдает -2 + 5 = " + actualValue);
     }
 }
